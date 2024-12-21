92d781eef3633a0e0eb0cd180614614b5be9f3b0
猿人易 <qcjxberin@outlook.com>
2024-12-21 18:18:46 +0800

    重构项目以支持宝塔面板API库
    修改了 `Pek.BaoTa.csproj` 文件，移除了 `net461` 和 `net472`，保留了 `netstandard2.0`、`netstandard2.1`、`net8.0` 和 `net9.0`。更新了 `RootNamespace` 和 `Description` 属性，并添加了对 `Pek.Common` 包的引用。
    
    更新了 `Readme.MD` 文件的标题和描述，以反映项目的新用途。
    
    新增了 `BaoTaHelper.cs` 文件，包含 `Pek.BaoTa` 命名空间和 `BaoTaHelper` 静态类，提供生成 MD5 哈希值和表单内容的方法。
    
    新增了 `BTSettings.cs` 文件，包含 `Pek.BaoTa` 命名空间和 `BTSettings` 配置类，提供宝塔面板地址和密钥的配置。

---
+++ Pek.BaoTa/BTSettings.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+
+using NewLife.Configuration;
+
+namespace Pek.BaoTa;
+
+/// <summary>宝塔参数配置</summary>
+[DisplayName("宝塔参数配置")]
+[Config("BT")]
+internal class BTSettings : Config<BTSettings>
+{
+    /// <summary>
+    /// 宝塔面板地址
+    /// </summary>
+    [Description("宝塔面板地址")]
+    public String? BtPanel { get; set; }
+
+    /// <summary>
+    /// 宝塔面板密钥
+    /// </summary>
+    [Description("宝塔面板密钥")]
+    public String? BtKey { get; set; }
+}
---
+++ Pek.BaoTa/BaoTaHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+using NewLife;
+
+namespace Pek.BaoTa;
+
+public static class BaoTaHelper {
+    /// <summary>
+    /// 签名实现
+    /// </summary>
+    /// <param name="input"></param>
+    /// <returns></returns>
+    public static String GetMd5Hash(String? input)
+    {
+        if (input.IsNullOrWhiteSpace()) return String.Empty;
+
+        var md5 = MD5.Create();
+        var inputBytes = Encoding.ASCII.GetBytes(input);
+        var hashBytes = md5.ComputeHash(inputBytes);
+        var sb = new StringBuilder();
+        foreach (var t in hashBytes)
+        {
+            sb.Append(t.ToString("x2"));
+        }
+
+        return sb.ToString();
+    }
+
+
+
+    public static FormUrlEncodedContent GetFormContent(List<KeyValuePair<String, String>>? data = null)
+    {
+        var requestTime = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+        var requestToken = GetMd5Hash(requestTime + GetMd5Hash(BTSettings.Current.BtKey));
+
+        data ??= [];
+
+        data.AddRange(
+        [
+            new KeyValuePair<String, String>("request_time", requestTime),
+            new KeyValuePair<String, String>("request_token", requestToken)
+        ]);
+        var formContent = new FormUrlEncodedContent(data);
+        return formContent;
+    }
+}
